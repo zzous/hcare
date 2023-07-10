22b6e01ec0b9642a2ddc3319333b886b8df1691e
YoonmiKim <yoonlemon20@gmail.com>
2023-07-11 02:35:36 +0900

    menulist

--- src/components/layout/MainMenu.vue
+++ src/components/layout/MainMenu.vue
@@ -2,25 +2,16 @@
     <div class="admin_menu">
         <div class="menu">
             <ul>
-                <!-- 1depth -->
-                <li 
-                    :class="[{'depth':item.depth},{'isActive':item.isActive}]"
-                    v-for="(item, index) in state.menu" :key="index"
-                >
-                    <span @click="gnbOpen(index)">{{ item.label }}</span>
-                    <!-- 2depth -->
-                    <ul v-if="item.depth">
-                        <li v-for="(sub, i) in item.depth_sub" :key="i" :class="[{'isActive':sub.isActive}]">
-                            <span @click="gnbOpen(index, i)">{{ sub.label }}</span>
-                             <!-- 3depth -->
-                            <ul>
-                                <li v-for="(depth, idx) in sub.menuitems" :key="idx" :class="[{'isActive':depth.isActive}]">
-                                    <span @click="gnbOpen(index, i, idx, url)">{{ depth.label }}</span>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
+                <template v-for="(item, index) in state.menuList" :key="index">
+                    <li>
+                        <span>{{ item.label }}</span>
+                        <ul v-if="item.children">
+                            <template v-for="(subItem, subIndex) in item.children" :key="subIndex">
+                                <li @click="linkTo(subItem.path)">{{ subItem.label }}</li>
+                            </template>
+                        </ul>
+                    </li>
+                </template>
             </ul>
         </div>
     </div>
@@ -28,18 +19,44 @@
 <script>
 import { reactive, watch } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
-import { menuList } from "@/data/menu.js";
+import { menuList } from '@/data/menu.js';
 export default {
     setup () {
         const router = useRouter()
         const route = useRoute()
         const state = reactive({
-            menu:menuList,
-            currentMenu:''
+            menuList: menuList,
+            currentMenu: ''
         })
+
+        /** @hooks */
+        watch(() => route, () => {
+            if(route.meta.children){
+                state.currentMenu = route.meta.children
+                state.menuList.forEach((item) => {
+                    if(item.label === state.currentMenu[0]){
+                        item.isActive = true
+                        item.depth_sub.forEach((sub) => {
+                            if(sub.label === state.currentMenu[1]){
+                                sub.isActive = true
+                                sub.menuitems.forEach((depth) => {
+                                    if(depth.label === state.currentMenu[2]){
+                                        depth.isActive =true
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+
+                console.log(state.currentMenu[1])
+            }
+        })
+
+        /** @methods */
         const gnbOpen =(depth1, depth2, depth3) =>{
             console.log(depth1, depth2, depth3)
-            state.menu.forEach((item, index) => {
+            state.menuList.forEach((item, index) => {
                 //depth1
                 if(index === depth1){
                     if(!item.depth){
@@ -70,37 +87,18 @@ export default {
                 
             })
         }
-        watch(() => {
-            //라우팅에 따른 메뉴 on 처리
-            if(route.meta.sublocation_depth){
-                router,
-                route
-                state.currentMenu = route.meta.sublocation_depth
-                state.menu.forEach((item) => {
-                    if(item.label === state.currentMenu[0]){
-                        item.isActive = true
-                        item.depth_sub.forEach((sub) => {
-                            if(sub.label === state.currentMenu[1]){
-                                sub.isActive = true
-                                sub.menuitems.forEach((depth) => {
-                                    if(depth.label === state.currentMenu[2]){
-                                        depth.isActive =true
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
 
-                console.log(state.currentMenu[1])
-            }
-        })
+        const linkTo = (link) => {
+            router.push(link)
+        }
+        
     
         
         return {
             state,
             menuList,
-            gnbOpen 
+            gnbOpen,
+            linkTo,
         }
     }
 }
--- src/data/menu.js
+++ src/data/menu.js
@@ -1,46 +1,19 @@
-export const menuLists =  [
-    {label: "회원관리", url:'', depth:true, isActive:false,
-        depth_sub:[
-            {label:"회원정보관리", url:'', isActive:false,
-                menuitems:[
-                    {label:"회원정보관리", url:"/memberinfo", isActive:false},
-                    {label:"자격상실명단관리", url:"/", isActive:false}
-                ]
-            },
-            {label:"휴먼회원관리", url:'', isActive:false,
-                menuitems:[
-                    {label:"회원정보관리", url:"/memberinfo", isActive:false},
-                    {label:"자격상실명단관리", url:"/", isActive:false}
-                ]
-            },
-            {label:"탈퇴회원관리",suburl:"/",isActive:false,
-                menuitems:[
-                    {label:"회원정보관리", url:"/memberinfo", isActive:false},
-                    {label:"자격상실명단관리", url:"/", isActive:false}
-                ]
-            }, 
-        ]
+export const menuList =  [
+    {
+        label: 'home',
+        path: '/'
     },
-    {label: "서비스 이용관리", url:'',  depth:true, isActive:false,
-        depth_sub:[
-            {label:"서비스 상품관리", url:'', isActive:false,
-                menuitems:[
-                    {label:"상품요청 관리", url:"/memberinfo", isActive:false},
-                    {label:"상품목록", url:"/", isActive:false}
-                ]
+    {
+        label: 'member',
+        children: [
+            {
+                label: 'sign-in',
+                path: '/login'
             },
-            {label:"루틴관리", url:'', isActive:false,
-                menuitems:[
-                    {label:"공개 루틴 관리", url:"/memberinfo", isActive:false},
-                    {label:"타임 루틴 관리", url:"/", isActive:false}
-                ]
-            },
-            {label:"탈퇴회원관리",suburl:"/",isActive:false,
-                menuitems:[
-                    {label:"회원정보관리", url:"/memberinfo", isActive:false},
-                    {label:"자격상실명단관리", url:"/", isActive:false}
-                ]
-            }, 
+            {
+                label: 'sign-up',
+                path: '/join'
+            }
         ]
     },
 ]
